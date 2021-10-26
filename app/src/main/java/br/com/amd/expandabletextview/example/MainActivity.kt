a8654ff28abc9b1473d7c91644c2e36c38a556b1
Aldo Dias <aldomddev@gmail.com>
2021-10-26 20:37:22 -0300

    Expanded/Collapsed lifecycle aware listeners added

--- app/src/main/java/br/com/amd/expandabletextview/example/MainActivity.kt
+++ app/src/main/java/br/com/amd/expandabletextview/example/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.amd.expandabletextview.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.com.amd.expandabletextview.example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,13 +13,36 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setViewListeners()
+    }
+
+    private fun setViewListeners() {
         with(binding) {
             btnSetLongText.setOnClickListener {
-                theText.text = getString(R.string.long_text)
+                theText.text = getString(R.string.long_text_1)
+            }
+
+            btnSetLongestText.setOnClickListener {
+                theText.text = getString(R.string.long_text_2)
             }
 
             btnSetShortText.setOnClickListener {
                 theText.text = getString(R.string.short_text)
+            }
+
+            theText.apply {
+                setAnimationDurationTime(
+                    lifecycleOwner = this@MainActivity,
+                    timeInMillis = 300
+                )
+
+                setOnCollapsedListener {
+                    Toast.makeText(this@MainActivity, "Collapsed!", Toast.LENGTH_SHORT).show()
+                }
+
+                setOnExpandedListener {
+                    Toast.makeText(this@MainActivity, "Expanded!", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
--- expandable-textview/src/main/java/br/com/amd/expandabletextview/ExpandableTextView.kt
+++ expandable-textview/src/main/java/br/com/amd/expandabletextview/ExpandableTextView.kt
@@ -15,12 +15,18 @@ import androidx.core.animation.doOnStart
 import androidx.core.content.withStyledAttributes
 import androidx.core.text.buildSpannedString
 import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+
+private typealias onStateChangeListener = (() -> Unit)
 
 class ExpandableTextView @JvmOverloads constructor (
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : AppCompatTextView(context, attrs, defStyleAttr),
+    LifecycleEventObserver,
     View.OnClickListener,
     ValueAnimator.AnimatorUpdateListener {
 
@@ -31,14 +37,17 @@ class ExpandableTextView @JvmOverloads constructor (
 
     private var attrExpandActionHint: String = ""
     private var attrCollapsedMaxLines = Int.MAX_VALUE
-    private var attrAnimationDurationInMillis = 0L
+
+    private var onExpandedListener: onStateChangeListener? = null
+    private var onCollapsedListener: onStateChangeListener? = null
 
     init {
         context.withStyledAttributes(attrs, R.styleable.ExpandableTextView) {
             val expandActionHint = getString(R.styleable.ExpandableTextView_expandActionHint).orEmpty()
-            attrExpandActionHint = "$DEFAULT_ACTION_HINT$expandActionHint"
-            attrCollapsedMaxLines = getInteger(R.styleable.ExpandableTextView_collapsedMaxLines, maxLines)
-            attrAnimationDurationInMillis = getInteger(R.styleable.ExpandableTextView_animationDurationInMillis, 0).toLong()
+            setExpandActionHint(expandActionHint)
+
+            val collapsedMaxLines = getInteger(R.styleable.ExpandableTextView_collapsedMaxLines, maxLines)
+            setCollapsedMaxLines(collapsedMaxLines)
         }
 
         setupView()
@@ -60,25 +69,53 @@ class ExpandableTextView @JvmOverloads constructor (
         toggleState()
     }
 
-    override fun onAnimationUpdate(animation: ValueAnimator?) {
-        val newHeightValue = animation?.animatedValue as? Int ?: height
-        updateHeight(animatedValue = newHeightValue)
+    override fun onAnimationUpdate(animation: ValueAnimator) {
+        val newHeightValue = animation.animatedValue as Int
+        updateHeight(newHeight = newHeightValue)
     }
 
-    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        super.onLayout(changed, left, top, right, bottom)
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
-        if (animator.isRunning) return
+        if (animator.isRunning || isUpdatingCollapsedText) {
+            return
+        }
 
         if (lineCount <= attrCollapsedMaxLines) {
-            text = originalText
             isClickable = false
-        } else if (isCollapsed() && !text.contains(attrExpandActionHint)) {
+        } else {
             isClickable = true
-            updateCollapsedText()
+            if (isCollapsed()) {
+                updateCollapsedText()
+            }
+        }
+    }
+
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        if (event == Lifecycle.Event.ON_DESTROY) {
+            onExpandedListener =  null
+            onCollapsedListener = null
+            animator.cancel()
         }
     }
     // endregion
+
+    fun setAnimationDurationTime(lifecycleOwner: LifecycleOwner, timeInMillis: Long = 300) {
+        if (lifecycleOwner.lifecycle.currentState == Lifecycle.State.DESTROYED) {
+            return
+        }
+
+        lifecycleOwner.lifecycle.addObserver(this)
+        setAnimationDuration(timeInMillis)
+    }
+
+    fun setOnExpandedListener(onExpanded: (() -> Unit)?) {
+        this.onExpandedListener = onExpanded
+    }
+
+    fun setOnCollapsedListener(onCollapsed: (() -> Unit)?) {
+        this.onCollapsedListener = onCollapsed
+    }
 
     private fun setupView() {
         originalText = text
@@ -88,7 +125,7 @@ class ExpandableTextView @JvmOverloads constructor (
 
     private fun setupAnimator() {
         animator = ValueAnimator.ofInt(0, 0).apply {
-            duration = attrAnimationDurationInMillis
+            duration = DEFAULT_ANIMATION_DURATION
             interpolator = AccelerateDecelerateInterpolator()
             addUpdateListener(this@ExpandableTextView)
             doOnStart { animationStarted() }
@@ -96,11 +133,30 @@ class ExpandableTextView @JvmOverloads constructor (
         }
     }
 
+    private fun setExpandActionHint(actionHint: String) {
+        attrExpandActionHint = "$DEFAULT_ACTION_HINT$actionHint"
+    }
+
+    private fun setCollapsedMaxLines(collapsedMaxLines: Int) {
+        attrCollapsedMaxLines = if (collapsedMaxLines > 0) {
+            collapsedMaxLines
+        } else {
+            DEFAULT_COLLAPSED_MAX_LINES
+        }
+    }
+
+    private fun setAnimationDuration(durationInMillis: Long) {
+        animator.duration = if (durationInMillis >= 0) {
+            durationInMillis
+        } else {
+            DEFAULT_ANIMATION_DURATION
+        }
+    }
+
     private fun animationStarted() {
         if (isCollapsed()) {
-            isCollapsing = false
-            text = originalText
             maxLines = Int.MAX_VALUE
+            text = originalText
         } else {
             isCollapsing = true
         }
@@ -108,9 +164,13 @@ class ExpandableTextView @JvmOverloads constructor (
 
     private fun animationEnded() {
         if (isExpanded() && isCollapsing) {
-            maxLines = attrCollapsedMaxLines
-            updateCollapsedText()
             isCollapsing = false
+            maxLines = attrCollapsedMaxLines
+            this.onCollapsedListener?.invoke()
+        }
+
+        if (isExpanded()) {
+            this.onExpandedListener?.invoke()
         }
 
         setWrapContent()
@@ -145,10 +205,8 @@ class ExpandableTextView @JvmOverloads constructor (
         return maxLines != Int.MAX_VALUE
     }
 
-    private fun updateHeight(animatedValue: Int) {
-        val layoutParams = layoutParams
-        layoutParams.height = animatedValue
-        setLayoutParams(layoutParams)
+    private fun updateHeight(newHeight: Int) {
+        layoutParams = layoutParams.apply { height = newHeight }
     }
 
     private fun updateCollapsedText() {
@@ -160,7 +218,7 @@ class ExpandableTextView @JvmOverloads constructor (
         val styleStart = hintReplaceStart + DEFAULT_ACTION_HINT.length
 
         val finalTextWithActionHint = buildSpannedString {
-            append(text)
+            append(originalText)
             replace(hintReplaceStart, visibleTextEnd, "$attrExpandActionHint\n")
             setSpan(UnderlineSpan(), styleStart, visibleTextEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
             setSpan(StyleSpan(Typeface.BOLD), styleStart, visibleTextEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
@@ -170,12 +228,12 @@ class ExpandableTextView @JvmOverloads constructor (
     }
 
     private fun setWrapContent() {
-        val layoutParams = layoutParams
-        layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
-        setLayoutParams(layoutParams)
+        layoutParams = layoutParams.apply { height = ViewGroup.LayoutParams.WRAP_CONTENT }
     }
 
     private companion object {
-        const val DEFAULT_ACTION_HINT = "... "
+        const val DEFAULT_ACTION_HINT = "${Typography.ellipsis} "
+        const val DEFAULT_COLLAPSED_MAX_LINES = 1
+        const val DEFAULT_ANIMATION_DURATION = 0L
     }
 }
